0d80e70fa046604fec24d5d50c5f0de044a6bce3
ParkDoil <poker5323@naver.com>
2023-03-28 22:01:09 +0900

    [Feat] 가장 가까운 적 도출

---
+++ Rogue-Lite/Assets/Scripts/Character/MonsterScan.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScan : MonoBehaviour
+{
+    [SerializeField]
+    private float _scanRange;
+    [SerializeField]
+    private LayerMask _target;
+    private RaycastHit2D[] _hits;
+    private Transform _nearTarget;
+
+    private void FixedUpdate()
+    {
+        _hits = Physics2D.CircleCastAll(transform.position, _scanRange, Vector2.zero, 0, _target);
+        _nearTarget = GetNearTarget();
+    }
+
+    private Transform GetNearTarget()
+    {
+        Transform answer = null;
+        float distance = 1000f;
+
+        foreach(RaycastHit2D target in _hits)
+        {
+            Vector3 myPos = transform.position;
+            Vector3 targetPos = target.transform.position;
+            float targetDistance = Vector3.Distance(myPos, targetPos);
+
+            if(targetDistance < distance)
+            {
+                distance = targetDistance;
+                answer = target.transform;
+            }
+        }
+
+        return answer;
+    }
+}
